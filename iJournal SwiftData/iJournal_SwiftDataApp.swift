8b6ae487f82ad3c3231b656a3c227a779366f4d1
Derek Wang <derekwangsz@gmail.com>
2024-01-16 21:10:24 -0800

    Initial Commit

---
+++ iJournal SwiftData/iJournal_SwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  iJournal_SwiftDataApp.swift
+//  iJournal SwiftData
+//
+//  Created by Derek Wang on 2024-01-16.
+//
+
+import SwiftUI
+
+@main
+struct iJournal_SwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
